a06a3c3658c7a3c9a5ac0215fe45c8dfada8e9a9
Eduardo Canellas de Oliveira <eduardo.canellas@bemobi.com>
2022-05-27 15:31:31 -0300

    remove usage of static mut

--- src/map_views.rs
+++ src/map_views.rs
@@ -5,7 +5,7 @@ pub struct DotView;
 impl DotView {
     pub fn to_dot(&self, mind_map: &MindMap) -> String {
         let mut nodes = String::new();
-        chain_nodes(&mut nodes, mind_map);
+        chain_nodes(&mut nodes, 0, mind_map);
 
         format!(
             r#"
@@ -29,34 +29,24 @@ edge [
     }
 }
 
-// TODO remove this gambiarra
-static mut CLUSTER_NUMBER: usize = 0;
-
-fn with_cluster(output: &mut String, f: impl FnOnce(&mut String, usize)) {
-    let cluster_number;
-
-    unsafe {
-        cluster_number = CLUSTER_NUMBER;
-        CLUSTER_NUMBER += 1;
-    }
-
+fn with_cluster(output: &mut String, level: usize, f: impl FnOnce(&mut String, usize)) {
     output.push_str(&format!(
-        "subgraph cluster_{cluster_number} {{ color=black;style=\"rounded\";"
+        "subgraph cluster_{level} {{ color=black;style=\"rounded\";"
     ));
 
-    f(output, cluster_number);
+    f(output, level);
 
     output.push('}');
 }
 
-fn chain_nodes(output: &mut String, mind_map: &MindMap) {
+fn chain_nodes(output: &mut String, level: usize, mind_map: &MindMap) {
     let content = &mind_map.content;
 
-    with_cluster(output, |output, _| {
+    with_cluster(output, level, |output, _| {
         output.push_str(&format!("\"{content}\";"));
     });
 
-    with_cluster(output, |output, number| {
+    with_cluster(output, level + 1, |output, number| {
         if let Some(child) = mind_map.children.first() {
             let child = &child.content;
             output.push_str(&format!(
@@ -70,6 +60,6 @@ fn chain_nodes(output: &mut String, mind_map: &MindMap) {
     });
 
     for map in mind_map.children.iter() {
-        chain_nodes(output, map);
+        chain_nodes(output, level + 1, map);
     }
 }
